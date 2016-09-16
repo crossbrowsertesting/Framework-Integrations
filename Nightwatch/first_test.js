1f11dfa977600afcf99e2162b64f5871e25e6d71
chasecook412 <chase@crossbrowsertesting.com>
2016-09-16 13:15:13 -0500

    new file:   Nightwatch/first_test.js 	new file:   Nightwatch/nightwatch.json 	new file:   Nightwatch/reports/first_test.xml

---
+++ Nightwatch/first_test.js
@@ -0,0 +1,13 @@
+this.testTodos = function(browser) {
+    browser
+      .url('http://crossbrowsertesting.github.io/todo-app.html')
+      .useXpath()
+      .click('/html/body/div/div/div/ul/li[4]/input')
+      .click('/html/body/div/div/div/ul/li[5]/input')
+      .setValue('//*[@id="todotext"]', 'run your first selenium test')
+      .click('//*[@id="addbutton"]')
+      .click('/html/body/div/div/div/a')
+      .useCss()
+      .assert.containsText('li:nth-child(4) > span', 'run your first selenium test')
+      .end();
+}
