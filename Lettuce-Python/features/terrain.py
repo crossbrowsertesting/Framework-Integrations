37db8c342057f971c75a1ac3fdf7970f69647384
chasecook412 <chase@crossbrowsertesting.com>
2016-09-30 10:03:58 -0500

    modified:   Lettuce-Python/features/terrain.py

--- Lettuce-Python/features/terrain.py
+++ Lettuce-Python/features/terrain.py
@@ -11,8 +11,14 @@ authkey = '12345'
 @before.all
 def setUp():
     caps = {}
-    caps['os_api_name'] = 'Win10'
-    caps['browser_api_name'] = 'Chrome49'
+    caps['name'] = 'First Lettuce Test'
+    caps['build'] = '1.0'
+    caps['browser_api_name'] = 'IE10'
+    caps['os_api_name'] = 'Win7x64-C2'
+    caps['screen_resolution'] = '1024x768'
+    caps['record_video'] = 'true'
+    caps['record_network'] = 'true'
+
     world.browser = webdriver.Remote(
         desired_capabilities = caps,
         command_executor="http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub"%(username, authkey)
