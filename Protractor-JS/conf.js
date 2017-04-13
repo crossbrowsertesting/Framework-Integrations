8d39eec84887f96c089eb13a5ab392b739b69768
chasecook412 <chase@crossbrowsertesting.com>
2017-04-13 11:35:27 -0500

    parallel configs

--- Protractor-JS/conf.js
+++ Protractor-JS/conf.js
@@ -1,29 +1,21 @@
 exports.config = {
 
   // change this to your USERNAME and AUTHKEY
-  seleniumAddress: 'http://you@yourdomain.com:yourauthkey@hub.crossbrowsertesting.com:80/wd/hub',
+  seleniumAddress: 'http://<username>:<authkey>@hub.crossbrowsertesting.com:80/wd/hub',
 
-  capabilities : {
-    
-    // this will show up in the UI
-    name : 'Selenium-Protractor',
+  // add more browsers to this array for more parllel tests!!
+  multiCapabilities: [{
+    'name': 'Protractor Parallel Example',
+    'browserName': 'chrome',
+    'browser_api_name': 'Chrome56x64',
+    'os_api_name': 'Win10'
+  }, {
+    'name': 'Protractor Parallel Example',
+    'browserName': 'firefox',
+    'browser_api_name': 'FF46x64',
+    'os_api_name': 'Win10'
+  }],
 
-    // these are important :)
-    browser_api_name : 'Chrome53x64', // change this according to what browser you are using
-    browserName: 'chrome', // change this according to what browser you are using 'internet explorer', 'chrome' etc
-    os_api_name : 'Win10', // change this for the OS you are using
-    screen_resolution : '1024x768', // change this for the resolution
-
-    // optional recording capabilities
-    record_video : 'true',
-    record_network : 'false',
-    record_snapshot : 'false',
-
-    // change this to your USERNAME and AUTHKEY
-    username :  "you@yourdomain.com",
-    password : "yourauthkey"
-
-  },
   //
-  specs: ['todo-spec.js']
+  specs: ['spec.js']
 };
